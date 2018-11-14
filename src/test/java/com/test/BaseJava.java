2c7645035ee197bb4d196caf36363652dc429851
xuanzg163 <xuan_zg@163.com>
2018-11-14 16:40:58 +0800

    产生随机UUID

--- src/test/java/com/test/BaseJava.java
+++ src/test/java/com/test/BaseJava.java
@@ -2,6 +2,8 @@ package com.test;
 
 import org.junit.Test;
 
+import java.util.UUID;
+
 /**
  * @author zhangxuan
  * @date 2018/11/5
@@ -24,6 +26,13 @@ public class BaseJava {
 
         b = x > y && ++x == --y; //x=6,y=8 (短路与，左边为false，右边不会计算)
         System.out.println(b + "   " + x + " ,  " + y);
+
+        /**
+         * 产生随机UUID
+         */
+        System.out.println(UUID.randomUUID());
+        //fc178143-d6f2-430f-8594-298249db7197
+        //fc178143-d6f2-430f-8594-298249db7197
     }
 
 }
